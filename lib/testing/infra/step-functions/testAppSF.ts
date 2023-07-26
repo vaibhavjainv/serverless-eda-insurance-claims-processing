a92ed6165851faec370c9e4f34f9107c19b0804a
Vaibhav Jain <jaivai@amazon.com>
2023-07-26 16:19:02 -0400

    Added table

--- lib/testing/infra/step-functions/testAppSF.ts
+++ lib/testing/infra/step-functions/testAppSF.ts
@@ -1,12 +1,20 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import { InputType, IntegrationPattern, JsonPath, LogLevel, StateMachine, StateMachineType } from "aws-cdk-lib/aws-stepfunctions";
+import {
+  InputType,
+  IntegrationPattern,
+  JsonPath,
+  LogLevel,
+  StateMachine,
+  StateMachineType,
+  Timeout,
+} from "aws-cdk-lib/aws-stepfunctions";
 import { Construct } from "constructs";
 import { LambdaInvocationType, LambdaInvoke } from "aws-cdk-lib/aws-stepfunctions-tasks";
 import * as logs from "aws-cdk-lib/aws-logs";
 import { RetentionDays } from "aws-cdk-lib/aws-logs";
-import { RemovalPolicy } from "aws-cdk-lib";
+import { Duration, RemovalPolicy } from "aws-cdk-lib";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 export interface TestApplicationSFProps {
@@ -37,18 +45,19 @@ export class TestApplicationSF extends StateMachine {
 function createSignUpLambdaStep(scope: Construct, props: TestApplicationSFProps): LambdaInvoke {
   return new LambdaInvoke(scope, "Customer Sign Up", {
     lambdaFunction: props.testLoginLambdaFunction,
-    payload:{
+    payload: {
       type: InputType.OBJECT,
-      value:{
+      value: {
         "userPoolId.$": "$.userPoolId",
         "identityPoolId.$": "$.identityPoolId",
         "clientId.$": "$.clientId",
         "userName.$": "$.userName",
         "password.$": "$.password",
         "postData.$": "$.postData",
-        "TaskToken": JsonPath.taskToken
-      }
+        TaskToken: JsonPath.taskToken,
+      },
     },
-    integrationPattern: IntegrationPattern.WAIT_FOR_TASK_TOKEN
+    integrationPattern: IntegrationPattern.WAIT_FOR_TASK_TOKEN,
+    taskTimeout: Timeout.duration(Duration.seconds(60)),
   });
 }
